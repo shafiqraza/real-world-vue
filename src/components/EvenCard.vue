d55081bcaa384f6b6906c6c37847cdce57e21ee1
shafiqraza <shafiqraza503@gmail.com>
2020-10-03 20:55:12 +0500

    Starting form noww

---
+++ src/components/EvenCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1></h1>
+</template>
+
+<script>
+export default {
+  props: {
+    propName: {
+      type: Number,
+      default: null
+    }
+  },
+  data() {
+    return {
+      quote: 'i Want to be single'
+    }
+  }
+}
+</script>
+
+<style></style>
