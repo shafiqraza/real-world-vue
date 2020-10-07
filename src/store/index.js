ca4918da8ac87a32cf96244d7b2cc4447cfdf372
Muhammad Shafiq <58416026+shafiqraza@users.noreply.github.com>
2020-10-07 14:16:19 -0700

    Learned state and getters

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,48 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 1, name: 'John' },
+    categories: [
+      'sustainablity',
+      'nature',
+      'animal walfare',
+      'housing',
+      'education',
+      'food',
+      'comumnity'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: true },
+      { id: 3, text: '...', done: false },
+      { id: 4, text: '...', done: true }
+    ],
+    events: [
+      { id: 1, text: '...', organizer: true },
+      { id: 2, text: '...', organizer: true },
+      { id: 3, text: '...', organizer: false },
+      { id: 4, text: '...', organizer: true }
+    ]
+  },
   mutations: {},
   actions: {},
-  modules: {}
-});
+  modules: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todos => todos.done).length
+    },
+    unDoneTodos: state => {
+      return state.todos.filter(todos => !todos.done).length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
+})
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,45 @@
 <template>
-  <h1>Create an event</h1>
+  <div>
+    <h1>Create list, {{ user.id }}</h1>
+    <p>This list is created by {{ user.name }}</p>
+    <p>There're {{ catLength }} categories:</p>
+
+    <!-- <ul>
+      <li v-for="cate in categories" :key="cate">{{ cate }}</li>
+    </ul> -->
+    <p>{{ doneTodos }} Todos are done and {{ unDoneTodos }} are not.</p>
+    <p>{{ getEventById(2) }}</p>
+  </div>
 </template>
+<script>
+// use mapState when you have retrive mutiple stuff from store.js becuse it becomes easy
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  // ** when you dont need other computed property
+  // computed: mapState({
+  //   user: 'user',
+  //   categories: 'categories'
+  // })
+
+  //**  but if need more computed properties not just from store.js
+  computed: {
+    //** retriving data from getters
+
+    // catLength() {
+    //   return this.$store.getters.catLength
+    // },
+    // doneTodos() {
+    //   return this.$store.getters.doneTodos
+    // },
+    // undoneTodos() {
+    //   return this.$store.getters.unDoneTodos
+    // },
+    // getEvent() {
+    //   return this.$store.getters.getEventById
+    // },
+    ...mapGetters(['getEventById', 'unDoneTodos', 'doneTodos', 'catLength']),
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
